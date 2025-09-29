5000e5eb8cb5b08317d49283e0607f02d3c8f53b
durgesh512s <your-email@example.com>
2025-09-29 17:45:26 +0530

    Fixed GTM cache issue

--- src/components/GoogleTagManager.tsx
+++ src/components/GoogleTagManager.tsx
@@ -25,7 +25,7 @@ export function GoogleTagManager({ gtmId = GTM_ID }: GoogleTagManagerProps = {})
             (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
             new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
             j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl+'&cb='+Date.now();f.parentNode.insertBefore(j,f);
             })(window,document,'script','dataLayer','${gtmId}');
           `,
         }}
@@ -43,7 +43,7 @@ export function GoogleTagManagerNoScript({ gtmId = GTM_ID }: GoogleTagManagerPro
   return (
     <noscript>
       <iframe
-        src={`https://www.googletagmanager.com/ns.html?id=${gtmId}`}
+        src={`https://www.googletagmanager.com/ns.html?id=${gtmId}&cb=${Date.now()}`}
         height="0"
         width="0"
         style={{ display: 'none', visibility: 'hidden' }}
